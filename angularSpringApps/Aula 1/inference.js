490bc1b5709eb776d6d69ca013f9d464b58cb785
Jessica Castro <jessica.castros@outlook.com>
2019-03-25 13:54:55 -0300

    Aula 1

---
+++ angularSpringApps/Aula 1/inference.js
@@ -0,0 +1,12 @@
+function showLog(message) {
+    console.log(message);
+}
+showLog('Olá, mundo!');
+showLog(21);
+/*
+* String
+* Number
+* Boolean
+* Arrays
+* Classes pessoais
+*/ 
---
+++ angularSpringApps/Aula 1/inference.ts
@@ -0,0 +1,17 @@
+function showLog(message: any) {
+    console.log(message);
+}
+
+showLog('Olá, mundo!');
+showLog(21);
+/*
+* String
+* Number
+* Boolean 
+* Arrays
+* Classes pessoais
+*/
+
+let test: Array<string> = ['Hello', 'Testes', 'Ops'];
+let isEnabled : boolean = true;
+
